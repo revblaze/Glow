de096fd03e095f7ef1457730764e74e2b8e4e0bc
Justin Bush <jaybush7@gmail.com>
2017-11-21 12:14:18 -0500

    Added ProgressBar; Fixed AddressBar & Background Bugs
    • Fixed AddressBar so that when clicked on, the full URL is displayed
    • When a page has loaded, the AddressBar will return to its original
    state using the prettyURL function (utilizes url.host)
    • Fixed background bugs so that the WKWebView is no longer static
    • Added ProgressBar with WKWebView estimatedProgress observer

--- macOS/Glow/ViewController.swift
+++ macOS/Glow/ViewController.swift
@@ -11,8 +11,10 @@ import WebKit
 
 class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTextFieldDelegate, NSTextDelegate {
     
+    // Main View Elements
     @IBOutlet var webView: WKWebView!
     @IBOutlet var addressBar: NSTextField!
+    @IBOutlet var progressBar: NSProgressIndicator!
     @IBOutlet var navConstraint: NSLayoutConstraint!
     @IBOutlet var sideConstraint: NSLayoutConstraint!
     
@@ -22,6 +24,59 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
     @IBOutlet var homeButton: NSButton!
     
     var searchRequest = ""
+    
+    override func viewWillAppear() {
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.activeAddressBarNotification(_:)), name: NSNotification.Name(rawValue: "ActiveAddressBar"), object: nil)
+        
+        let gradient = CAGradientLayer()
+        
+        gradient.colors = [NSColor.blue.cgColor,
+                           NSColor.red.cgColor,
+                           NSColor.green.cgColor]
+        
+        gradient.locations = [0, 0.5, 1.0]
+        
+        gradient.frame = view.frame
+
+        //view.layer.mask = gradient
+        progressBar.layer?.mask = gradient
+        //progressBar.controlTint = gradient
+        progressBar.layer?.borderColor = CGColor.clear
+        progressBar.layer?.backgroundColor = CGColor.clear
+        progressBar.layer?.compositingFilter = gradient
+        progressBar.wantsLayer = true
+        
+        progressBar.isBezeled = false
+        
+        addressBar.resignFirstResponder()
+        
+    }
+    
+    override func viewWillDisappear() {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    // Observer for WebView Loading Progress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            DispatchQueue.main.async {
+                self.setProgress(self.webView.estimatedProgress * 100)
+            }
+        }
+    }
+    
+    // Set Progress for ProgressBar
+    fileprivate func setProgress(_ value: Double) {
+        if value == 100 {
+            self.progressBar.isHidden = true
+            return
+        }
+        
+        self.progressBar.isHidden = false
+        self.progressBar.doubleValue = value
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,21 +101,35 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
         // WebView Debug
         let request = URL(string: "https://medium.com")!
         webView.load(URLRequest(url: request))
+        webView.setValue(false, forKey: "drawsBackground")
+        webView.enclosingScrollView?.backgroundColor = NSColor.clear
         
-        addressBar.refusesFirstResponder = true
     }
     
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         webView.isHidden = false
+        progressBar.isHidden = false
         //updateAddressBar(input: "")
         deselectAdressBar()
         addressBar.refusesFirstResponder = true
+        //webView.becomeFirstResponder()
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        progressBar.isHidden = true
+        self.setProgress(100)
+        
         let niceURL = prettyURL(input: webView.url! as NSURL)
         updateAddressBar(input: niceURL)
         deselectAdressBar()
+        
+        webView.becomeFirstResponder()
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.setProgress(0)
+        //addressBar.refusesFirstResponder = true
+        addressBar.resignFirstResponder()
     }
     
     // AddressBar: Load Request
@@ -89,6 +158,22 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
         }
     }
     
+    @IBAction func refreshPage(_: Any?) {
+        webView.reload()
+    }
+    
+    @IBAction func showSideMenu(_: Any?) {
+        addressBar.isHidden = false
+        navConstraint.constant = 40.0
+        sideConstraint.constant = 40.0
+    }
+    
+    @IBAction func hideSideMenu(_: Any?) {
+        addressBar.isHidden = true
+        navConstraint.constant = 0.0
+        sideConstraint.constant = 0.0
+    }
+    
     // AddressBar: Update URL
     func updateAddressBar(input: String) {
         if (input.isEmpty) {
@@ -105,6 +190,8 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
         
         if prettyURL?.lowercased().range(of:"google.") != nil {
             prettyURL = searchRequest
+        } else if prettyURL?.lowercased().range(of:"www.") != nil {
+            prettyURL = prettyURL?.replacingOccurrences(of: "www.", with: "", options: .literal, range: nil)
         }
         
         return prettyURL!
@@ -116,9 +203,16 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
         return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: input)
     }
     
+    @objc func activeAddressBarNotification(_ aNotification: Notification) {
+        print("Being Called?")
+        print((webView.url?.absoluteString)!)
+        addressBar.stringValue = (webView.url?.absoluteString)!
+        print(addressBar.stringValue)
+    }
+    
     // AddressBar: User Began Editing AddressBar
     override func controlTextDidBeginEditing(_ aNotification: Notification) {
-        addressBar.stringValue = (webView.url?.absoluteString)!
+        //addressBar.stringValue = (webView.url?.absoluteString)!
     }
     
     // AddressBar: User Finished Editing AddressBar
@@ -160,8 +254,8 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
 
 }
 
-
 extension String {
+    
     func isEmptyOrSpaces() -> Bool {
         
         if (self.isEmpty) {
@@ -172,15 +266,11 @@ extension String {
 }
 
 extension NSTextField {
-    override open func becomeFirstResponder() -> Bool {
-        NotificationCenter.default.post(name: .activeAddressBar, object: NSTextField())
+    
+    open override func becomeFirstResponder() -> Bool {
+        NotificationCenter.default.post(name: Notification.Name("ActiveAddressBar"), object: nil)
         return true
     }
-
-}
-
-extension Notification.Name {
     
-    static let activeAddressBar = Notification.Name("activeAddressBar")
 }
 
